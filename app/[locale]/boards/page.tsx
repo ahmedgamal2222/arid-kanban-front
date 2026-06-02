69d054b616a436d3d82fa08277142021e7f97cf6
gemya <gemya@AHMED>
2026-06-02 07:08:25 +0300

    Fix: change boards page to force-static for static export

--- app/[locale]/boards/page.tsx
+++ app/[locale]/boards/page.tsx
@@ -1,6 +1,6 @@
 import BoardsDashboard from './BoardsDashboard';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 
 export default function BoardsPage() {
   return <BoardsDashboard />;
